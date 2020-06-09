e0cf2b93fcabbe7868245643ffefc28c1b9e32d9
Mike <mwroberts89@gmail.com>
2020-06-09 00:00:26 -0400

    first versions

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+from cs50 import SQL
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    with open('pptest.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+
+        # counts the number of entries in imported csv and creates hashtable of dictionaries
+        count = 0
+        raw_data = []
+        for row in csv_reader:
+            raw_data.append(row)
+            count += 1
+        
+        # determines number of total weeks in csv file
+        total_weeks = int(count/7)
+
+        # sets up a 2D array a different list for every week of the csv provided
+        weeks = [[] for j in range(total_weeks + 1)]
+
+        
+        # adds data to weeks hashtable in groups of seven
+        day_count = 0
+        week_count = 0
+        tomato_count = 0
+        for row in raw_data:
+            day_count += 1
+            tomato_count += int(row['tomatoes'])
+            weeks[week_count].append(row)
+            
+            # determines if seven days has been counted
+            if day_count % 7 == 0:
+                week_count += 1
+                day_count = 0
+                # seems like I can make a temporary dictionary with same names as week's keys, so this can be displayed in the home.html template
+                end_of_week_stats = {'date' : week_count, 'tomatoes' : tomato_count, 'task' : round(tomato_count/7, 2)}
+                total_weeks -= 1
+                weeks[week_count].append(end_of_week_stats)
+                tomato_count = 0
+
+            # copy table exactly, so it can be displayed in reverse on webpage
+            full_chart = []
+            for week in weeks:
+                for day in week:
+                    full_chart.append(day)
+
+        
+        # print(weeks)
+            
+
+        return render_template('home.html', full_chart = full_chart, week_count = week_count, tomato_count = tomato_count, day_count = day_count)
+
+# print('test')
