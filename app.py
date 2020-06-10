6b344c35be4c43bb72256afcdf18546ef804a20f
Mike <mwroberts89@gmail.com>
2020-06-09 23:23:49 -0400

    starting to transfer database from csv to sqlite, added load page

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, abort
+from flask_session import Session
+from tempfile import mkdtemp
 from cs50 import SQL
 import csv
 
 app = Flask(__name__)
 
-@app.route('/')
+# Configure session to use filesystem (instead of signed cookies)
+# really not sure what this does, but just copied from CS50 finance python file
+    # looks like there's some more info here https://www.reddit.com/r/cs50/comments/bc4x9t/help_understanding_session_config_with/
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# opens the app's database
+db = SQL("sqlite:///pomodoro.db")
+
+# dictionary used to store the table id that user wants to use.
+session = {"table_id" : "user input"}
+
+
+@app.route('/', methods=["GET", "POST"])
+def load():
+    if request.method == "POST":
+        # TODO want users to have the option to load table or create one
+        return redirect('/home')
+    else:
+        return render_template("load.html")
+
+@app.route('/home')
 def home():
     with open('pptest.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
